c089d64b72035353dae681ce6a1d1ac3dda59277
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-10-31 02:51:36 +0530

    update models

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 require('dotenv').config();
+require('./Models/db')
 const PORT =process.env.PORT||8080;
 
 app.get('/',(req,res)=>{
     res.send('hello')
 })
-
+ 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
