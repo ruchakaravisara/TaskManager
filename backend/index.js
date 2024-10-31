b215c93becd1240bec13da1b33574b82f9d81dce
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-10-31 07:57:18 +0530

    checking the get endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ const app = express();
 require('dotenv').config();
 require('./Models/db')
 const PORT =process.env.PORT||8080;
+const TaskRouter =require('./Routes/TaskRouter')
 
 app.get('/',(req,res)=>{
     res.send('hello')
-})
+}); 
+
+app.use('/tasks',TaskRouter);
  
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
